cbf47a62c6119accb8b785613a68cdf9ce272d4d
愛知巧規 <prosenyou123@gmail.com>
2018-01-19 18:26:54 +0900

    Stage1で、フェードインしてから始まるようにした

---
+++ Assets/Application/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	//フェードアウト処理
+	[SerializeField]
+	Image fadeInImage;
+	private float fadeInValue=255;
+	private bool is_fadeIn = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fadeInValue > 0) FadeIn ();
+	}
+
+	//フェードインしてゲーム開始
+	void FadeIn(){
+		fadeInValue-= 3f;
+		if (fadeInValue > 0) {
+			fadeInImage.color = new Color (0/255,0/255,0/255,fadeInValue/255);
+		}
+		Debug.Log ("ふぇーどいんなう");
+	}
+
+}
